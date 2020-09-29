3294ba6d7d82dbc29c79761ceb01edccef6114d7
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-09-29 22:28:04 +0530

    Fixes wheel rotation

--- src/components/wheel/wheel.js
+++ src/components/wheel/wheel.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 
 const Wheel = () => {
-  console.log(document.getElementById("wheel-rotate"));
+  const [center, setCenter] = useState({ x: 0, y: 0 });
+  const [animateAngle, setAnimateAngle] = useState(0);
+  const [duration, setDuration] = useState(0.001);
+  const refEl = useRef(null);
+
+  useEffect(() => {
+    if (center.x === 0) {
+      const {
+        offsetHeight,
+        offsetTop,
+        offsetWidth,
+        offsetLeft,
+      } = refEl.current;
+      setCenter({
+        x: offsetWidth / 2 + offsetLeft,
+        y: offsetHeight / 2 + offsetTop,
+      });
+    }
+  });
+
+  useEffect(() => {
+    if (center.x !== 0) {
+      let active = false;
+      let R2D = 180 / Math.PI;
+      let startAngle = 0;
+      let angle = 0;
+      let rotation = 0;
+      refEl.current.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        angle += rotation;
+        return (active = false);
+      });
+      refEl.current.addEventListener(
+        "mousedown",
+        function (e) {
+          e.preventDefault();
+          const x = e.clientX - center.x;
+          const y = e.clientY - center.y;
+          startAngle = R2D * Math.atan2(y, x);
+          return (active = true);
+        },
+        false
+      );
+
+      refEl.current.addEventListener("mousemove", (e) => {
+        if (!active) return;
+        e.preventDefault();
+        let x = e.clientX - center.x;
+        let y = e.clientY - center.y;
+        let d = R2D * Math.atan2(y, x);
+        rotation = d - startAngle;
+        console.log(rotation);
+        return (refEl.current.style.webkitTransform = `rotate(${
+          angle + rotation
+        }deg)`);
+      });
+    }
+  }, [center]);
   return (
     <div className="wheel-container">
-      <div className="wheel">
-        <div className="inner-wheel" id="wheel-rotate">
+      <div className="wheel" ref={refEl}>
+        <motion.div
+          animate={{ rotate: animateAngle }}
+          transition={{ duration }}
+          className="inner-wheel"
+          id="wheel-rotate"
+        >
           <div className="sec">
             <span className="fa fa-bell-o"></span>
           </div>
@@ -30,10 +93,10 @@ const Wheel = () => {
           <div className="sec">
             <span className="fa fa-lightbulb-o"></span>
           </div>
-        </div>
-        <div className="spin-container" id="spin">
-          <div className="spin-inner" />
-        </div>
+        </motion.div>
+      </div>
+      <div className="spin-container" id="spin">
+        <div className="spin-inner" />
       </div>
     </div>
   );
