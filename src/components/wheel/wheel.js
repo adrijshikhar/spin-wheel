16cd1520d159f18cc111d997a6009cd76679f297
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-09-29 17:56:40 +0530

    Adds spin wheel

---
+++ src/components/wheel/wheel.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Wheel = () => {
+  return (
+    <div className="wheel-container">
+      <div className="wheel" id="wheel-rotate">
+        <div className="spin-container">
+          <div className="spin-inner" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Wheel;
