60333f720d0fd2a184a99a1c552ec830f726b3de
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-09-30 19:24:22 +0530

    Adds prod

---
+++ build/precache-manifest.32d29f89a17b8c2bc3ec542662070aac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c140848dd5b4c643a5524b1ef0bf36f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6051b2fc99bea4740c1e",
+    "url": "/static/css/3.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "704ec824c82acdddd9f8",
+    "url": "/static/css/main.64253fd2.chunk.css"
+  },
+  {
+    "revision": "03358533e92e52481e28",
+    "url": "/static/js/2.cd6a6d6a.chunk.js"
+  },
+  {
+    "revision": "f1133377457596a1f7013455ae1b442a",
+    "url": "/static/js/2.cd6a6d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6051b2fc99bea4740c1e",
+    "url": "/static/js/3.e5610af0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.e5610af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca87a1e7d444d10dad5c",
+    "url": "/static/js/4.2aa19541.chunk.js"
+  },
+  {
+    "revision": "84e4f12b256e2a747c5c",
+    "url": "/static/js/5.70cb050f.chunk.js"
+  },
+  {
+    "revision": "704ec824c82acdddd9f8",
+    "url": "/static/js/main.db164977.chunk.js"
+  },
+  {
+    "revision": "1d84a0c2b2c65ab6178f",
+    "url": "/static/js/runtime-main.03de6723.js"
+  },
+  {
+    "revision": "c9052976e063161a469889bd78956744",
+    "url": "/static/media/Metropolis-Bold.c9052976.woff"
+  },
+  {
+    "revision": "67ef196488282fd58ff05a84701eecc8",
+    "url": "/static/media/Metropolis-Regular.67ef1964.woff"
+  },
+  {
+    "revision": "042e59350825c0f73dcdccab500b5e55",
+    "url": "/static/media/Metropolis-SemiBold.042e5935.woff"
+  }
+]);
